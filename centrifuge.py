89b108f5aa0f7fb0df02d4b5699ffc18d6de62e0
TheCasualNerd <tyler.duck02@gmail.com>
2017-11-04 15:02:13 -0400

    Add files via upload

---
+++ centrifuge.py
@@ -0,0 +1,23 @@
+from wpilib.joystick import Joystick
+"""This Module """
+import numpy as np
+
+class Centrifuge(Joystick):
+    def __init__(self, port):
+        super(Centrifuge, self).__init__()
+    def pow_scale(self, val, pow):
+        _abs_v = abs(val)
+        _sgn_v = np.sign(val)
+        return _sgn_v * (_abs_v**pow)
+
+
+    def _pow_scale(self, val):
+        return self.pow_scale(val, 2.0)
+
+
+    def getButton(self, port):
+        return self.getRawButton(port)
+
+
+    def getAxis(self, port):
+        return self._pow_scale(self.getRawAxis(port))
